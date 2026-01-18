93a923e04484e6f39464aaeca84fe74dfdfe41d1
YadavJaismeen23 <167276476+YadavJaismeen23@users.noreply.github.com>
2026-01-18 22:17:57 +0530

    Initial pathology backend

--- config/session.js
+++ config/session.js
@@ -1,10 +1,13 @@
 const session = require("express-session");
 
 module.exports = session({
-  secret: "aarogyam-secret-key",
+  name: "aarogyam.sid", // custom session cookie name
+  secret: process.env.SESSION_SECRET || "aarogyam-dev-secret",
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 1000 * 60 * 60 // 1 hour
+    httpOnly: true,
+    secure: false, // true only in HTTPS (Render will handle later)
+    maxAge: 60 * 60 * 1000 // 1 hour
   }
 });
